bd8aa96b038e222b2d1211616b970481c38ff192
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-05 17:01:25 -0500

    feat(ui): add material design ui library with primevue.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,49 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
-
-createApp(App).mount('#app')
+import {PrimeVue} from '@primevue/core';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import Material from '@primeuix/themes/material';
+import {
+    Button,
+    Card,
+    Checkbox,
+    Column, ConfirmationService,
+    ConfirmDialog,
+    DataTable,
+    Dialog, DialogService, Drawer,
+    FileUpload, FloatLabel, IconField, InputIcon, InputNumber, InputText, Menu, Rating, Row,
+    Select,
+    SelectButton, Tag, Textarea, Toast, ToastService, Toolbar
+} from "primevue";
+ 
+createApp(App)
+    .use(PrimeVue, { theme: { preset: Material}, ripple: true})
+    .use(ConfirmationService)
+    .use(DialogService)
+    .use(ToastService)
+    .component('pv-button', Button)
+    .component('pv-card', Card)
+    .component('pv-column', Column)
+    .component('pv-confirm-dialog', ConfirmDialog)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-select', Select)
+    .component('pv-select-button', SelectButton)
+    .component('pv-file-upload', FileUpload)
+    .component('pv-float-label', FloatLabel)
+    .component('pv-icon-field', IconField)
+    .component('pv-input-icon', InputIcon)
+    .component('pv-input-text', InputText)
+    .component('pv-input-number', InputNumber)
+    .component('pv-menu', Menu)
+    .component('pv-rating', Rating)
+    .component('pv-row', Row)
+    .component('pv-drawer', Drawer)
+    .component('pv-tag', Tag)
+    .component('pv-textarea', Textarea)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-toast', Toast)
+    .mount('#app')
