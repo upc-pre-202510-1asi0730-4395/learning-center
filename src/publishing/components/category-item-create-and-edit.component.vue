b6188ce24d3d29d3a462ccaee65599cc73d60433
Angel Velasquez <pcisavel@upc.edu.pe>
2025-05-07 14:01:31 -0500

    feat(categories): add category item create and edit component.

---
+++ src/publishing/components/category-item-create-and-edit.component.vue
@@ -0,0 +1,41 @@
+<script>
+import CreateAndEdit
+
+export default {
+  name: "category-item-create-and-edit",
+  components: {CreateAndEdit},
+  props: {
+    item: null,
+    visible: false,
+  },
+  emits: ['cancel-requested', 'save-requested'],
+  data() {
+    return {
+      submitted: false,
+    }
+  },
+  methods: {
+    onCancelRequested() {
+      this.$emit('cancel-requested');
+    },
+    onSaveRequested() {
+      this.submitted = true;
+      this.$emit('save-requested', this.item);
+    },
+  }
+}
+</script>
+
+<template>
+<create-and-edit :entity="item" :visible="visible" entity-name="Category"
+                 @cancel-action-requested="onCancelRequested"
+                  @save-action-requested="onSaveRequested">
+  <template #content>
+    
+  </template>
+</create-and-edit>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/shared/components/create-and-edit.component.vue
@@ -0,0 +1,56 @@
+<script>
+
+const defaultStyle = { width: '400px'};
+
+export default {
+  name: "create-and-edit",
+  props: {
+    entity:     null,
+    visible:    Boolean(),
+    entityName: '',
+    edit:       Boolean(),
+    size:       'default',
+  },
+  emits: ['cancel-action-requested', 'save-action-requested'],
+  methods: {
+    onCancel() {
+      this.$emit('cancel-action-requested');
+    },
+    onSave() {
+      this.$emit('save-action-requested', this.entity);
+    },
+    getHeaderTitle() {
+      return `${this.edit ? 'Edit' : 'New'} ${this.entityName}`;
+    },
+    getSubmitLabel() {
+      return this.edit ? 'Update' : 'Create';
+    },
+    getDialogStyle() {
+      let dialogStyle = defaultStyle;
+      dialogStyle = this.size === 'standard' ? { width: '600px'} : defaultStyle;
+      dialogStyle = this.size === 'large' ? { width: '900px'} : defaultStyle;
+      return dialogStyle;
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-dialog  v-bind:visible="visible" :aria-modal="true" :StyleClass="getDialogStyle()" class="p-fluid" :header="entityName">
+    <template #header>
+      <div class="flex justify-content-start">
+        <div>{{ getHeaderTitle() }}</div>
+      </div> 
+    </template>
+    <slot name="content"></slot>
+    <template #footer>
+      <div class="flex justify-content-end">
+        <pv-button  type="button" :label="getSubmitLabel()" class="p-button-text" icon="pi pi-check" @click="onSave" />
+        <pv-button type="button" label="Cancel" severity="secondary" class="p-button-text" icon="pi pi-times" @click="onCancel" />
+      </div>
+  </pv-dialog>
+</template>
+
+<style scoped>
+
+</style>
